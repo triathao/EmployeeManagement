4c1d428ccabd391d2cd11142b1cc77e5244277fc
Tria Thao <triathao@gmail.com>
2021-01-02 15:51:46 -0800

    modifications

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,30 @@
 DROP DATABASE IF EXISTS employee_tracker_db;
-CREATE database employee_tracker_db;
+
+CREATE DATABASE employee_tracker_db;
 
 USE employee_tracker_db;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT,
-    department VARCHAR(30),
-    PRIMARY KEY (id)
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
 );
 
+-- Create the table role.
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INT,
-    PRIMARY KEY (id)
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,3),
+  department_id INT,
+  PRIMARY KEY (id)
 );
 
-CREATE table employee (    
-    id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT,
-    PRIMARY KEY (id)
+-- Create the table employee.
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT NULL,
+  PRIMARY KEY (id)
 );
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const cTable = require('console.table');
