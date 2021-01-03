a589725941019cb1d68d2fceba2bfb0d7bda0b92
Tria Thao <triathao@gmail.com>
2021-01-02 16:10:54 -0800

    functions

--- server.js
+++ server.js
@@ -156,3 +156,148 @@ function addEmployee() {
     );
   });
 }
+
+function addRole() {
+  inquirer
+  .prompt([
+    {
+      name: "newRole",
+      type: "input",
+      message: "Role title adding?"
+    },
+    {
+      name: "salary",
+      type: "input",
+      message: "Role salary adding?"
+    },
+    {
+      name: "depID",
+      type: "input",
+      message: "Role ID adding?"
+    }
+  ])
+  .then(function(answer) {
+    connection.query(
+      "INSERT INTO role SET ?",
+      {
+        title: answer.newRole,
+        salary: answer.salary,
+        department_id: answer.depID,
+      },
+      function(err) {
+        if (err) throw err;
+        console.log("Role added successfully");
+        action();
+      }
+    );
+  });
+}
+
+function addDepartment() {
+  inquirer
+  .prompt([
+    {
+      name: "newDep",
+      type: "input",
+      message: "Department name adding?"
+    }
+  ])
+  .then(function(answer) {
+    connection.query(
+      "INSERT INTO department SET ?",
+      {
+        name: answer.newDep,
+      },
+      function(err) {
+        if (err) throw err;
+        console.log("Department added successfully");
+        action();
+      }
+    );
+  });
+}
+
+function deleteEmployee() {
+    connection.query("SELECT * FROM employee", function(err, results) {
+        if (err) throw err;
+        inquirer
+          .prompt([
+            {
+              name: "choice",
+              type: "rawlist",
+              choices: function() {
+                var choiceArray = [];
+                for (var i = 0; i < results.length; i++) {
+                  choiceArray.push(`${results[i].first_name} ${results[i].last_name}`);
+                }
+                return choiceArray;
+              },
+              message: "Which employee would you like to remove?"
+            }
+          ])
+          .then(function(answer) {
+                var chosenEmployee;
+                for (var i = 0; i < results.length; i++) {
+                if (`${results[i].first_name} ${results[i].last_name}` === answer.choice) {
+                    chosenEmployee = results[i];
+                }
+                }
+                connection.query(
+                    "DELETE FROM employee WHERE ?",
+                    [
+                    {
+                        id: chosenEmployee.id
+                    }
+                    ],
+                    function(err) {
+                    if (err) throw err;
+                    console.log("Employee removed successfully!");
+                    action();
+                    }
+                );
+            });
+        });  
+}
+
+// Delete Roles
+function deleteRole() {
+    connection.query("SELECT * FROM role", function(err, results) {
+        if (err) throw err;
+        inquirer
+          .prompt([
+            {
+              name: "choice",
+              type: "rawlist",
+              choices: function() {
+                var choiceArray = [];
+                for (var i = 0; i < results.length; i++) {
+                  choiceArray.push(results[i].title);
+                }
+                return choiceArray;
+              },
+              message: "Which role would you like to remove?"
+            }
+          ])
+          .then(function(answer) {
+                var chosenRole;
+                for (var i = 0; i < results.length; i++) {
+                if (results[i].title === answer.choice) {
+                    chosenRole = results[i];
+                }
+                }
+                connection.query(
+                    "DELETE FROM role WHERE ?",
+                    [
+                    {
+                        id: chosenRole.id
+                    }
+                    ],
+                    function(err) {
+                    if (err) throw err;
+                    console.log("Role removed successfully!");
+                    action();
+                    }
+                );
+            });
+        });  
+}
