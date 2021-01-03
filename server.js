d59ce153411b899b5040deb20defbc59256b5cd5
Tria Thao <triathao@gmail.com>
2021-01-02 16:02:37 -0800

    prompts

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ function init() {
         type: "list",
         message: "What would you like to do?",
         choices: [
+            "View joined tables", 
             "View Employees",
             "View Roles",
             "View Departments",
@@ -39,8 +40,11 @@ function init() {
         ]
     })
 
-    .then((answer) => {
+    .then(function(answer) {
         switch (answer.action) {
+            case "View joined tables":
+          viewJoined();
+                break;
             case "View Employees":
                 viewEmployees();
                 break;
@@ -77,4 +81,78 @@ function init() {
     
             }
         })
+}
+
+function viewJoined() {
+    connection.query("SELECT role.title, role.salary, employee.first_name, employee.last_name, manager_id, department.name FROM ((employee_tracker_db.role INNER JOIN employee_tracker_db.employee ON role.id = employee.role_id) INNER JOIN employee_tracker_db.department ON role.department_id = department.id)", (err, results) => {
+        if (err) throw err
+        console.table(results)
+        action()
+    })
+}
+
+function viewEmployees() {
+    connection.query("SELECT * FROM employee", (err, results) => {
+        if (err) throw err
+        console.table(results)
+        action()
+    })
+}
+
+function viewRoles() {
+    connection.query("SELECT * FROM role", (err, results) => {
+        if (err) throw err
+        console.table(results)
+        action()
+    })
+}
+
+function viewDepartments() {
+    connection.query("SELECT * FROM department", (err, results) => {
+        if (err) throw err
+        console.table(results)
+        action()
+    })
+}
+
+function addEmployee() {
+  inquirer
+  .prompt([
+    {
+      name: "fName",
+      type: "input",
+      message: "First name of employee?"
+    },
+    {
+      name: "lName",
+      type: "input",
+      message: "Last name of employee?"
+    },
+    {
+      name: "roleID",
+      type: "input",
+      message: "Employee ID?"
+    },
+    {
+      name: "manID",
+      type: "input",
+      message: "Manager ID for the employee?"
+    },
+  ])
+  .then(function(answer) {
+    connection.query(
+      "INSERT INTO employee SET ?",
+      {
+        first_name: answer.fName,
+        last_name: answer.lName,
+        role_id: answer.roleID,
+        manager_id: answer.manID,
+      },
+      function(err) {
+        if (err) throw err;
+        console.log("Employee added successfully");
+        action();
+      }
+    );
+  });
 }
